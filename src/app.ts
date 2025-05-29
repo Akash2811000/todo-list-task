fd23fff48b14f828d2aadfc371fda5bceeb3cd7e
Akash2811000 <akashgupta7383@gmail.com>
2025-05-29 22:40:20 +0530

    feat: setup CRON job to mark expired todos as completed

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import connectDB from "./config/db";
+import { createExpiredTodosCronJob } from "./cronJobs/expiredTodos";
 import authRoutes from "./routes/auth.routes";
 import todoRoutes from "./routes/todo.routes";
-import swaggerUi from "swagger-ui-express";
 import * as swaggerDocument from "./swagger/swagger.json";
+
 const app = express();
 
 app.use(express.json());
+
 connectDB();
+
+createExpiredTodosCronJob();
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument), () => {
   console.log("swagger");
 });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
-
-app.get("/", (_, res) => {
-  res.send("TODO LIST");
-});
 
 export default app;
---
+++ src/cronJobs/expiredTodos.ts
@@ -0,0 +1,35 @@
+import cron from "node-cron";
+import Todo from "../models/todo.model";
+
+export const createExpiredTodosCronJob = (
+  cronExpression: string = "*/30 * * * * *" // run after 30 second
+) => {
+  return cron.schedule(cronExpression, async () => {
+    try {
+      const currentDate = new Date();
+      const result = await Todo.updateMany(
+        {
+          dueDate: { $lt: currentDate },
+          completed: false,
+        },
+        {
+          $set: {
+            completed: true,
+            updatedAt: currentDate,
+          },
+        }
+      );
+
+      console.log(
+        `[${new Date().toISOString()}] Expired todos cleanup: Updated ${
+          result.modifiedCount
+        } todos`
+      );
+    } catch (error) {
+      console.error(
+        `[${new Date().toISOString()}] Error in expired todos cleanup:`,
+        error
+      );
+    }
+  });
+};
