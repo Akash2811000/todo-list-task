1fac13f140b958d4f747f0dbabea9790b5c07213
Akash2811000 <akashgupta7383@gmail.com>
2025-05-29 20:06:27 +0530

    feat: connect MongoDB using Mongoose with initial connection handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import connectDB from "./config/db";
 
 const app = express();
 
 app.use(express.json());
-
+connectDB();
 app.get("/", (_, res) => {
   res.send("TODO LIST");
 });
